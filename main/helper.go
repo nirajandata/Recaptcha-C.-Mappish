7d437efee3e5e290a6d64e979f0e9320bccdc647
Nirajan Dhakal <nirajan.data@gmail.com>
2023-01-20 09:13:57 -0500

    json parser

--- main/helper.go
+++ main/helper.go
@@ -1,8 +1,20 @@
 package main
 
+import (
+  "encoding/json"
+)
 
-//make a map of url of thisxyzdoesn't exist
-//
-//3d array of picking element and setting them in string like nononooo(n for no, o for yes
-//json api to send that image;
-//
+func test(query string){
+  url="https://api.unsplash.com/search/photos?query="+query+"&per_page=6&client_id=gK52De2Tm_dL5o1IXKa9FROBAJ-LIYqR41xBdlg3X2k"
+  jsondata,err=http.Get(url)
+  if(err!=nil){
+    print("error")
+    return
+  }
+  var data []mp[string] inferface{}
+  json.Unmarshal([]byte(jsondata),&data)
+  var imglink []string
+  for _,value:=range data{
+    results:=data["results"].(map[string]interface{})
+  }
+}
