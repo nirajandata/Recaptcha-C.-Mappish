0420bdffe9bddc388ec7f7e40262a1bd0e1312d8
Nirajan Dhakal <nirajan.data@gmail.com>
2023-01-21 06:46:40 -0500

    chore:minor change

--- main/helper.go
+++ main/helper.go
@@ -3,33 +3,46 @@ package main
 import (
   "encoding/json"
   "net/http"
-  "net/http/httputil"
+  "io/ioutil"
   "math/rand"
   "os"
   "log"
 )
+
 func cerr(err error){
   if(err!=nil){
     log.Fatal(err)
+    os.Exit(0)
   }
 }
-func parser(query string, pg int) []string{
-  cid:=os.Getenv("CID")
-  url:="https://api.unsplash.com/search/photos?query="+query+"&per_page="+string(pg)+"&client_id="+cid
+func parser(query,pgg string) []string{
+  cid:=string(os.Getenv("CID"))
+  print(cid)
+  url:="https://api.unsplash.com/search/photos?query="+query+"&per_page="+pgg+"&client_id="+cid
   jsondatas,err:=http.Get(url)
   cerr(err)
-  jsondata,errs:=httputil.DumpResponse(jsondatas,true)
-  cerr(err)
-	var data []map[string]interface{}
-  json.Unmarshal([]byte(jsondata),&data)
+  jsondata,errs:=ioutil.ReadAll(jsondatas.Body)
+  cerr(errs)
+//  log.Printf("json data: %s\n", jsondata)
+  var val map[string]interface{}
+  err2:=json.Unmarshal([]byte(jsondata),&val)
+  cerr(err2)
   var imglink []string
-  for _,value:=range data{
-    results:=value["results"].(map[string]interface{})
-    urls:=results["urls"].(map[string]string)
+  result:=val["results"].(map[string]interface{})
+  urls:=result["urls"].(map[string]interface{})
+  print(urls["raw"])
+  imglink=append(imglink,"test")
+  /*
+  for _,value:=range val{
+    result:=value["results"].(map[string]interface{})
+    urls:=result["urls"].(map[string]string)
     imglink=append(imglink,urls["raw"])
+    print(imglink[0])
   }
+  */
   return imglink
 }
+
 func imageCreate(w http.ResponseWriter, r *http.Request){
 	n:=3
 	problist:=[3][3]int{}
@@ -47,17 +60,18 @@ func imageCreate(w http.ResponseWriter, r *http.Request){
 		problist[i][x]=target
 	}
 	var api Images
-	targetImg:=parser(names[target],3)
+	targetImg:=parser(names[target],"3")
 	tcount:=0
 	for i:=0;i<n;i++ {
 		for j:=0;j<n;j++{
 		text:=names[problist[i][j]]
+		print(text)
 		if(problist[i][j]==target){
 			api.urls=append(api.urls,targetImg[tcount])
 			tcount+=1
 		} else{
-			randomImg:=parser(text,1)
-			api.urls=append(api.urls,randomImg[0])
+//			randomImg:=parser(text,"1")
+//			api.urls=append(api.urls,randomImg[0])
 		}
 		}
 	}
---
+++ main/test.go
@@ -0,0 +1,10 @@
+package main
+
+
+func tester(){
+  print("test for parser..... \n")
+  a:=parser("mouse","1")
+  for i:=0; i<len(a);i++{
+    print("this is " +a[i])
+  }
+}
