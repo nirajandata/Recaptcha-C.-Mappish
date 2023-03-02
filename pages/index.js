775f8b36a473216d4388e22269b764bed3c0f64d
Nirajan Dhakal <nirajan.data@gmail.com>
2023-03-02 03:24:13 -0500

    remove:dummy images

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 var checkbox=document.getElementById("checkbox")
 //todo: to fetch json api from golang server
 //don't test anything, it's in active phase of early development
-images=["something.jpg","dont_test_now.jpg","underdevelopment.jpg"]
+var url ="http://localhost:8080/getimg"
+
+//images=["something.jpg","dont_test_now.jpg","underdevelopment.jpg"]
+
+images=[]
+async funtion imgLoader(){
+  const response=await fetch(url)
+  const result=await response.json()
+  images=result.img
+}
+imgLoader()
 for(let i=0;i<6;i++){
   let img=document.createElement("img")
   img.src=images[i]
